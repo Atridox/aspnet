18370cfa24b063dd49b6f636304e5cc6f0a6a96d
Atridox <152039379+Atridox@users.noreply.github.com>
2023-11-27 01:49:40 +0100

    Delete Models directory

--- Models/Race.cs
+++
@@ -1,9 +0,0 @@
-public class Race
-{
-
-    public int Id { get; set; }
-    public string WinnerName { get; set; }
-    public string WinnerTime { get; set; }
-    public string GrandPrix { get; set; }
-    public int NumberOfLaps { get; set; }
-}
--- Models/Team.cs
+++
@@ -1,8 +0,0 @@
-public class Team
-{
-    public int Id { get; set; }
-    public string Manufacturer { get; set; }
-    public string Driver1 { get; set; }
-    public string Driver2 { get; set; }
-    public string Image { get; set; }
-}
